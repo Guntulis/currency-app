f37f3fbea3c78d8df5106a3a129103fff36ee1b7
Rytis Guntulis <ryts@mobonapps.com>
2020-02-22 19:24:24 +0200

    Add retrofit call adapter

--- app/src/main/java/com/example/currencyapp/dagger/module/AppModule.kt
+++ app/src/main/java/com/example/currencyapp/dagger/module/AppModule.kt
@@ -5,6 +5,7 @@ import com.example.currencyapp.data.api.ApiClient
 import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
@@ -15,6 +16,7 @@ class AppModule {
     @Singleton
     fun providesRetrofit(): Retrofit {
         return Retrofit.Builder()
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .baseUrl(BuildConfig.API_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
